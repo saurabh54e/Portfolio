961323ff9df62462dc7a2d17313a63ff52b10912
Saurabh Singh <saurabhsingh505152@gmail.com>
2026-04-27 13:07:40 +0530

    added reveal feature

--- script.js
+++ script.js
@@ -124,3 +124,16 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+reveals.forEach(el => observer.observe(el));
